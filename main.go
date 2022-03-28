7e22849c4ba3d78d9c196b56ec2c5c272558dc6a
Pavle Galantić <pavle@flocksocial.io>
2022-03-28 22:38:40 +0200

    thoughts

--- main.go
+++ main.go
@@ -5,12 +5,21 @@ import (
 	"strings"
 )
 
+var arity = 2
+
 func main() {
 	input := "desi brate? kako si danas ti meni? nadam se da si dobro. inace, voleo bih da se druzimo nekad. nedostajes mi!"
 	a := strings.FieldsFunc(input, split)
-	for i := 0; i < len(a); i++ {
-		fmt.Println(a[i])
+	for i := 0; i < len(a)-arity; i++ {
+		fmt.Println(a[i : i+arity])
 	}
+
+	// the key of the map for the bag of words could be a string
+	// the checking of its existence can either be done with contains (checking if the words of the key are the same)
+	// or the words being
+
+	// log2arity - sorting the words to check the key (using O(log2n) sort)
+	// arity^2 - contains
 }
 
 func split(r rune) bool {
